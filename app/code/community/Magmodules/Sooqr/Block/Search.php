ae084c4a2c31c68f743382509313ec6de1af58e4
Magmodules <info@magmodules.eu>
2017-07-12 12:35:43 +0200

    1.2.1

--- app/code/community/Magmodules/Sooqr/Block/Search.php
+++ app/code/community/Magmodules/Sooqr/Block/Search.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Magmodules.eu - http://www.magmodules.eu
  *
@@ -18,6 +17,7 @@
  * @copyright     Copyright (c) 2017 (http://www.magmodules.eu)
  * @license       http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
+
 class Magmodules_Sooqr_Block_Search extends Mage_Core_Block_Template
 {
 
